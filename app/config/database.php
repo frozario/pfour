a062e8872c2f883a638b8bdeaf0b773c2b2de904
Francis Rozario <f.rozario@hotmail.com>
2014-11-26 11:34:03 -0500

    Second commit

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'juice_keeper',
 			'username'  => 'root',
-			'password'  => 'root',
+			'password'  => 'erJE24afx7',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
